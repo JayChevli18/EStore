d5eb5c1fe3e92d33f53267136b692e3ce96d9682
JayChevli18 <chevlijay70@gmail.com>
2025-01-29 19:00:12 +0530

    Added Auth

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+
+export default function SignUpPage(){
+    return(
+        <div className="h-screen flex justify-center items-center">
+            <SignUp></SignUp>
+        </div>
+    )
+}
